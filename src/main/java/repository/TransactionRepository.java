4c78c0294ce47456d5126452f3a36cef63619f48
Jemyun6 <jemyun6@gmail.com>
2023-02-14 17:25:24 +0100

    Update TransactionRepository.java

--- src/main/java/repository/TransactionRepository.java
+++ src/main/java/repository/TransactionRepository.java
@@ -1,5 +1,8 @@
 package repository;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -8,10 +11,22 @@ import domain.Transaction;
 
 @Repository
 public class TransactionRepository {
+	private List<Transaction> transactions = new ArrayList<>();
 
-	public List<Transaction> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Transaction> findAll() { 
+		return transactions;
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public void populateData() {
+		try (FileInputStream fileInputStream = new FileInputStream("transactions.txt");
+				 ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);){
+			//this.transactions = (List<Transaction>) objectInputStream.readObject();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 }
